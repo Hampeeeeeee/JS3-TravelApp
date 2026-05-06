aab7c0b1bc261dd115ebd55dcb20e361b532c0e5
Hampeeeeeee <hampus_svensson99@hotmail.com>
2026-05-06 14:27:34 +0200

    feat(): Added start quiz button linking to quiz page, comments, and layout | Examensarbete

--- src/pages/Flagged.tsx
+++ src/pages/Flagged.tsx
@@ -1,7 +1,11 @@
 import LoadSpinner from "@/components/loadspinner";
 import type { Country } from "@/components/types/Country";
 import { useQuery } from "@tanstack/react-query";
+import { useMemo } from "react";
+import "./startQuizBtn.css";
+import { Link } from "react-router";
 
+// Fetching of flags
 export default function Flagged() {
   const {
     data: countries = [],
@@ -22,13 +26,24 @@ export default function Flagged() {
     },
   });
 
-  const independentCountries = countries.filter((c) => c.independent === true);
+  // Variable to hold independent countries and randomize their positions
+  const independentCountries = useMemo(() => {
+    return countries
+      .filter((c) => c.independent === true)
+      .sort(() => Math.random() - 0.5);
+  }, [countries]);
 
+  // Rendered component
   return (
     <section className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold mb-32">
+      <div className="flex items-center justify-between mb-32">
+      <h1 className="text-3xl font-bold text-glow">
         Flagged! Welcome to the flag quiz!
       </h1>
+      <Link to="/quiz" className="startQuizBtn rounded border text-2xl font-bold border-glow">
+        Start Quiz
+      </Link>
+      </div>
       {isLoading && <LoadSpinner className="mx-auto mt-10" />}
       {isError && (
         <p className="text-center mt-10 text-primary">
@@ -36,21 +51,25 @@ export default function Flagged() {
         </p>
       )}
       {!isLoading && !isError && (
+        <>
+        <span></span>
         <div className="grid grid-cols-4 gap-4">
           {independentCountries.map((country) => (
             <div
-              key={country.name.common}
-              className="flex flex-col items-center"
+            key={country.name.common}
+            className="flex flex-col items-center"
             >
+              <p className="font-bold mb-2 w-full truncate">{country.name.common}</p>
               <img
                 src={encodeURI(country.flags?.svg || country.flags?.png || "")}
                 alt={country.name.common}
-                className="w-full h-32 object-cover rounded border border-blue-400"
+                className="w-full h-38 object-cover rounded border border-blue-400"
                 loading="lazy"
-              />
+                />
             </div>
           ))}
         </div>
+        </>
       )}
     </section>
   );
